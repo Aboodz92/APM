b2ed8702457842575dd3acc8f5c0056aa2bd6dae
Abdulrahman Omar <abdulrhmanomar3@gmail.com>
2019-05-07 14:01:40 +0400

    start the files

--- APM/dist/APM/main.js
+++ APM/dist/APM/main.js
@@ -41,7 +41,7 @@ module.exports = "thead {\r\n    color: #337ab7\r\n}\r\n/*# sourceMappingURL=dat
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class='card'>\r\n    <div class='card-header'>\r\n        {{pageTitle}}\r\n    </div>\r\n\r\n    <div class='card-body'>\r\n        <div class='row'>\r\n            <div class='col-md-2'> Filter by: </div>\r\n            <div class='col-md-4'>\r\n                <input type='text'\r\n                [(ngModel)]='listFilter' />\r\n            </div>\r\n        </div>\r\n        <div class='row'>\r\n            <div class='col-md-6'>\r\n                <h4>Filtered by: {{listFilter}}</h4>\r\n\r\n            </div>\r\n        </div>\r\n\r\n        <div class='table-responsive'>\r\n            <table class='table' *ngIf='products && products.length'>\r\n                <thead>\r\n                    <tr>\r\n                        <th><button class='btn btn-primary'\r\n                            (click)='toggleImage()'>\r\n                            {{showImage? 'Hide' : 'Show'}} Image\r\n                        </button></th>\r\n                        <th>Product</th>\r\n                        <th>Code</th>\r\n                        <th>Available</th>\r\n                        <th>Price</th>\r\n                        <th>5 Start Rating</th>\r\n                    </tr>\r\n                </thead>\r\n                \r\n                <tbody>\r\n                    <tr *ngFor='let product of products'>\r\n                        <td>\r\n                            <img *ngIf='showImage'\r\n                             [src]='product.imageUrl' \r\n                            [title]='product.productName' \r\n                            [style.width.px]='imageWidth'\r\n                            [style.margin.px]='imageMargin'>\r\n                        </td>\r\n                        <td>{{product.productName}}</td>\r\n                        <td>{{product.productCode | lowercase | ConvertToSpacesPipe: '-'}}</td>\r\n                        <td>{{product.ReleaseDate}}</td>\r\n                        <td>{{product.Description}}</td>\r\n                        <td>{{product.price | currency:'USD':'symbol':'1.2-2'}}</td>\r\n                        <td>{{product.starRating}}</td>\r\n\r\n                    </tr>\r\n                </tbody>\r\n            </table>\r\n\r\n        </div>\r\n\r\n    </div>\r\n</div>"
+module.exports = "<div class='card'>\r\n    <div class='card-header'>\r\n        {{pageTitle}}\r\n    </div>\r\n\r\n    <div class='card-body'>\r\n        <div class='row'>\r\n            <div class='col-md-2'> Filter by: </div>\r\n            <div class='col-md-4'>\r\n                <input type='text'\r\n                [(ngModel)]='listFilter' />\r\n            </div>\r\n        </div>\r\n        <div class='row'>\r\n            <div class='col-md-6'>\r\n                <h4>Filtered by: {{listFilter}}</h4>\r\n\r\n            </div>\r\n        </div>\r\n\r\n        <div class='table-responsive'>\r\n            <table class='table' *ngIf='products && products.length'>\r\n                <thead>\r\n                    <tr>\r\n                        <th><button class='btn btn-primary'\r\n                            (click)='toggleImage()'>\r\n                            {{showImage? 'Hide' : 'Show'}} Image\r\n                        </button></th>\r\n                        <th>Product</th>\r\n                        <th>Code</th>\r\n                        <th>Available</th>\r\n                        <th>Product Description</th>\r\n                        <th>Price</th>\r\n                        <th>5 Start Rating</th>\r\n                    </tr>\r\n                </thead>\r\n                \r\n                <tbody>\r\n                    <tr *ngFor='let product of filterProducts'>\r\n                        <td>\r\n                            <img *ngIf='showImage'\r\n                             [src]='product.imageUrl' \r\n                            [title]='product.productName' \r\n                            [style.width.px]='imageWidth'\r\n                            [style.margin.px]='imageMargin'>\r\n                        </td>\r\n                        <td>{{product.productName}}</td>\r\n                        <td>{{product.productCode | lowercase | ConvertToSpacesPipe: '-'}}</td>\r\n                        <td>{{product.ReleaseDate}}</td>\r\n                        <td>{{product.Description}}</td>\r\n                        <td>{{product.price | currency:'USD':'symbol':'1.2-2'}}</td>\r\n                        <!-- <td>{{product.starRating | number}} <pm-star></pm-star></td> -->\r\n                        <td><pm-star [rating] = 'product.starRating'\r\n                            (ratingClicked) = 'onRatingClicked($event)'></pm-star></td> <!-- We passing the parameter to the nested star compinent-->\r\n\r\n                    </tr>\r\n                </tbody>\r\n            </table>\r\n\r\n        </div>\r\n\r\n    </div>\r\n</div>"
 
 /***/ }),
 
@@ -62,6 +62,9 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 
 var ProductListComponent = /** @class */ (function () {
     function ProductListComponent() {
@@ -69,7 +72,6 @@ var ProductListComponent = /** @class */ (function () {
         this.imageWidth = 50;
         this.imageMargin = 2;
         this.showImage = false;
-        this.listFilter = 'cart';
         this.products = [
             {
                 "productId": 2,
@@ -82,7 +84,7 @@ var ProductListComponent = /** @class */ (function () {
                 "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
             },
             {
-                "productId": 6,
+                "productId": 5,
                 "productName": "Hammer",
                 "productCode": "TBX-0048",
                 "ReleaseDate": "May 21, 2016",
@@ -92,7 +94,29 @@ var ProductListComponent = /** @class */ (function () {
                 "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
             }
         ];
+        this.filterProducts = this.products;
+        this._listFilter = 'cart';
     }
+    Object.defineProperty(ProductListComponent.prototype, "listFilter", {
+        get: function () {
+            return this._listFilter;
+        },
+        set: function (value) {
+            this._listFilter = value;
+            this.filterProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ProductListComponent.prototype.onRatingClicked = function (message) {
+        this.pageTitle = 'Product List: ' + message;
+    };
+    ProductListComponent.prototype.performFilter = function (filterBy) {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter(function (product) {
+            return product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1;
+        });
+    };
     ProductListComponent.prototype.toggleImage = function () {
         this.showImage = !this.showImage;
     };
@@ -104,7 +128,8 @@ var ProductListComponent = /** @class */ (function () {
             selector: 'pm-products',
             template: __webpack_require__(/*! ./product-list.component.html */ "./src/app/Product/product-list.component.html"),
             styles: [__webpack_require__(/*! ./product-list.component.css */ "./src/app/Product/product-list.component.css")]
-        })
+        }),
+        __metadata("design:paramtypes", [])
     ], ProductListComponent);
     return ProductListComponent;
 }());
@@ -163,13 +188,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _Product_product_list_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./Product/product-list.component */ "./src/app/Product/product-list.component.ts");
-/* harmony import */ var _shared_convert_to_spaces_pipe__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./shared/convert-to-spaces.pipe */ "./src/app/shared/convert-to-spaces.pipe.ts");
+/* harmony import */ var _shared_star_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./shared/star.component */ "./src/app/shared/star.component.ts");
+/* harmony import */ var _shared_convert_to_spaces_pipe__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./shared/convert-to-spaces.pipe */ "./src/app/shared/convert-to-spaces.pipe.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -184,7 +211,8 @@ var AppModule = /** @class */ (function () {
             declarations: [
                 _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"],
                 _Product_product_list_component__WEBPACK_IMPORTED_MODULE_4__["ProductListComponent"],
-                _shared_convert_to_spaces_pipe__WEBPACK_IMPORTED_MODULE_5__["ConvertToSpacesPipe"]
+                _shared_convert_to_spaces_pipe__WEBPACK_IMPORTED_MODULE_6__["ConvertToSpacesPipe"],
+                _shared_star_component__WEBPACK_IMPORTED_MODULE_5__["StarComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -230,6 +258,81 @@ var ConvertToSpacesPipe = /** @class */ (function () {
         })
     ], ConvertToSpacesPipe);
     return ConvertToSpacesPipe;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/shared/star.component.css":
+/*!*******************************************!*\
+  !*** ./src/app/shared/star.component.css ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ".crop {\r\n  overflow: hidden;\r\n}\r\ndiv {\r\n  cursor: pointer;\r\n}\r\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvc2hhcmVkL3N0YXIuY29tcG9uZW50LmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLGdCQUFnQjtBQUNsQjtBQUNBO0VBQ0UsZUFBZTtBQUNqQiIsImZpbGUiOiJzcmMvYXBwL3NoYXJlZC9zdGFyLmNvbXBvbmVudC5jc3MiLCJzb3VyY2VzQ29udGVudCI6WyIuY3JvcCB7XHJcbiAgb3ZlcmZsb3c6IGhpZGRlbjtcclxufVxyXG5kaXYge1xyXG4gIGN1cnNvcjogcG9pbnRlcjtcclxufSJdfQ== */"
+
+/***/ }),
+
+/***/ "./src/app/shared/star.component.html":
+/*!********************************************!*\
+  !*** ./src/app/shared/star.component.html ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"crop\"\r\n     [style.width.px]=\"starWidth\"\r\n     [title]=\"rating\"\r\n     (click)=\"onClick()\">\r\n  <div style=\"width: 75px\">\r\n    <span class=\"fa fa-star\"></span>\r\n    <span class=\"fa fa-star\"></span>\r\n    <span class=\"fa fa-star\"></span>\r\n    <span class=\"fa fa-star\"></span>\r\n    <span class=\"fa fa-star\"></span>\r\n  </div>\r\n</div>"
+
+/***/ }),
+
+/***/ "./src/app/shared/star.component.ts":
+/*!******************************************!*\
+  !*** ./src/app/shared/star.component.ts ***!
+  \******************************************/
+/*! exports provided: StarComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StarComponent", function() { return StarComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var StarComponent = /** @class */ (function () {
+    function StarComponent() {
+        this.ratingClicked = new _angular_core__WEBPACK_IMPORTED_MODULE_0__["EventEmitter"]();
+    }
+    StarComponent.prototype.ngOnChanges = function () {
+        this.starWidth = this.rating * 75 / 5;
+    };
+    StarComponent.prototype.onClick = function () {
+        this.ratingClicked.emit(" The rating " + this.rating + " was clicked! ");
+    };
+    __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"])(),
+        __metadata("design:type", Number)
+    ], StarComponent.prototype, "rating", void 0);
+    __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Output"])(),
+        __metadata("design:type", _angular_core__WEBPACK_IMPORTED_MODULE_0__["EventEmitter"])
+    ], StarComponent.prototype, "ratingClicked", void 0);
+    StarComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'pm-star',
+            template: __webpack_require__(/*! ./star.component.html */ "./src/app/shared/star.component.html"),
+            styles: [__webpack_require__(/*! ./star.component.css */ "./src/app/shared/star.component.css")]
+        })
+    ], StarComponent);
+    return StarComponent;
 }());
 
 
@@ -297,7 +400,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\the__\OneDrive\Desktop\APM\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Github\APM\APM\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- APM/dist/APM/polyfills.js
+++ APM/dist/APM/polyfills.js
@@ -5501,8 +5501,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\the__\OneDrive\Desktop\APM\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\the__\OneDrive\Desktop\APM\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Github\APM\APM\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Github\APM\APM\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
